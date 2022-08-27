1abf6a1f627bdd5dc68be7cd232c24b1f8c00183
Jose Luiz Martins <luizjls@gmail.com>
2022-08-27 18:48:01 -0300

    range sign

--- range.py
+++ range.py
@@ -1,4 +1,5 @@
 
+'''
 mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for n in range(51,60):
@@ -8,3 +9,21 @@ for n in range(51,60):
 # range(1,5)   -->  [1, 2, 3, 4]
 # range(1,3)   -->  [1, 2]
 # range(1,10)  -->  [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for n in range(5):
+    print(n)
+
+for n in range(2,5):
+    print(n)
+
+for n in range(0,50,5):
+    print(n)
+
+for n in range(10, -10, -1):
+    print(n)
+
+'''
+
+for n in range(10, -10, -1):
+    print(n)
+
