a17b421866fd8aef7d900911049d4d5015ceb5ed
Jose Luiz Martins <luizjls@gmail.com>
2022-09-19 18:45:56 -0300

    class sep, 19

--- list.py
+++ list.py
@@ -8,8 +8,6 @@ else:
     print(f'O numero {num} nao foi encontrado' )
 
 
-'''
-
 list1 = []
 list2 = list(range(5,20))
 list3 = ['P','e','r','s','o','n','a','l','D','a','d']
@@ -29,6 +27,23 @@ list5.pop()
 print(list5)
 print(list6)
 
+
+list = [1, 2, 3, 4, 5, 6, 7]
+
+print(sum(list))
+print(max(list))
+print(min(list))
+print(len(list))
+
+'''
+
+list = [1, 2, 3, 4, 5, 6, 7]
+print(list)
+print(type(list))
+
+tupla = tuple(list)
+print(tupla)
+print(type(tupla))
 
 
 
---
+++ tuple.py
@@ -0,0 +1,31 @@
+'''
+tp = tuple(range(7, 20, 2))
+print(tp)
+print(type(tp))
+
+tp = 'Victor', "Erik", "Luiz"
+print(tp)
+
+b1, b2, b3 = tp
+print(b1)
+print(b2)
+
+
+tp = (1, 2, 3, 4, 5, 6, 7, "Erik")
+
+print(sum(tp))
+print(max(tp))
+print(min(tp))
+print(len(tp))
+
+'''
+
+meses = ('JAN', 'FEV', 'MAR', 'ABR', 'MAI', 'JUN', 'JUL', 'AGO', 'SET', 'OUT', 'NOV', 'DEZ')
+
+i = 0
+while i < len(meses):
+    print(meses[i])
+    i = i + 1
+
+
+
